7be8cca18791056a6c3b807b1be34347bb98c5e9
mathetjegift <simonmatgee@gmail.com>
2024-04-17 08:46:04 +0200

    Added a loader

--- js/script.js
+++ js/script.js
@@ -1,48 +1,6 @@
-// //news=======================================================
-//  const url = 'https://www.businesslive.co.za/rss/?publication=bl&section=news';
 
-// async function getData() {
-//     try {
-//       const response = await fetch(url);
-//       const data = await response.json();
-//       // console.log(data);
-      
-//       // Store fetched articles
-//       const articles = data.results;
-  
-    
-//       let currentIndex = 0;
-  
-//       // Display the next article
-//       function displayNextArticle() {
-//         const newsContainer = document.querySelector('.news');
-//         newsContainer.innerHTML = ''; // Clear current content
-  
-//         const article = articles[currentIndex];
-//         const articleElement = document.createElement('div'); //putting the news in a div
-//         articleElement.classList.add('news-article'); //new class
-//         articleElement.innerHTML = `
-//           <h2>${article.title}</h2>
-   
-         
-//         `;
-//         newsContainer.appendChild(articleElement);
-  
-//         // Increment index or loop back to the first article
-//         currentIndex = (currentIndex + 1) % articles.length;
-//       }
-  
-//       // Initial display
-//       displayNextArticle();
-  
-//       // timer for the slide
-//       setInterval(displayNextArticle, 5000); // Show a new article every 5 seconds
-//     } catch (error) {
-//       console.error('Error fetching news data:', error);
-//     }
-//   }
-  
-//   getData();
-
-
-
+window.addEventListener('load', function () {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.content').style.display = 'block';
+    document.body.style.overflow = 'auto'; // Restore scrolling after content is loaded
+});
